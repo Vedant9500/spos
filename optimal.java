680755d860125ebe025d58d669b632f462ee361e
dnyaneshwardagade10 <dnyaneshwardagade07@gmail.com>
2025-11-07 23:48:19 +0530

    Update optimal.java

--- optimal.java
+++ optimal.java
@@ -6,47 +6,68 @@ public class OptimalPageReplacement {
         List<Integer> frames = new ArrayList<>();
         int pageFaults = 0;
 
+        System.out.println("\n--- Optimal Page Replacement ---");
+
         for (int i = 0; i < pages.length; i++) {
             int page = pages[i];
 
             if (!frames.contains(page)) {
                 pageFaults++;
+
                 if (frames.size() < frameSize) {
                     frames.add(page);
                 } else {
-                    // Find the page to replace
                     int indexToReplace = -1;
                     int farthestUse = -1;
 
-                    for (int j = 0; j < frames.size(); j++) {
-                        int f = frames.get(j);
+                    // Find the page used farthest in the future
+                    for (int f = 0; f < frames.size(); f++) {
+                        int currentPage = frames.get(f);
                         int nextUse = Integer.MAX_VALUE;
 
-                        // Check future uses
-                        for (int k = i + 1; k < pages.length; k++) {
-                            if (pages[k] == f) {
-                                nextUse = k;
+                        // Check when the current frame page will be used next
+                        for (int j = i + 1; j < pages.length; j++) {
+                            if (pages[j] == currentPage) {
+                                nextUse = j;
                                 break;
                             }
                         }
 
+                        // The page not used for the longest time (or never used) will be replaced
                         if (nextUse > farthestUse) {
                             farthestUse = nextUse;
-                            indexToReplace = j;
+                            indexToReplace = f;
                         }
                     }
+
                     frames.set(indexToReplace, page);
                 }
             }
-            System.out.println("Frames after accessing " + page + ": " + frames);
+
+            System.out.println("Page: " + page + "  ->  Frames: " + frames);
         }
 
         System.out.println("\nTotal Page Faults: " + pageFaults);
     }
 
     public static void main(String[] args) {
-        int[] pages = {2, 3, 2, 1, 5, 2, 4, 5, 3, 2, 5, 2};
-        int frameSize = 3;
+        Scanner sc = new Scanner(System.in);
+
+        // Frame size input
+        System.out.print("Enter number of frames: ");
+        int frameSize = sc.nextInt();
+
+        // Number of pages input
+        System.out.print("Enter number of pages: ");
+        int n = sc.nextInt();
+
+        int[] pages = new int[n];
+
+        // Page reference string
+        System.out.println("Enter page reference string:");
+        for (int i = 0; i < n; i++) {
+            pages[i] = sc.nextInt();
+        }
 
         optimalPageReplacement(pages, frameSize);
     }
