025913c871cc5929f580103e0590ede4534f5f13
Sarthak Chavan <144462055+sarthak11234@users.noreply.github.com>
2025-11-07 23:09:52 +0530

    Update ReaderWriter.java

--- Mutex/ReaderWriter.java
+++ Mutex/ReaderWriter.java
@@ -1,27 +1,20 @@
 import java.util.concurrent.locks.*;
 
 public class ReaderWriter {
-    private static int readCount = 0;
-
-    private static final Lock mutex = new ReentrantLock();     // For readCount
-    private static final Lock writeLock = new ReentrantLock(); // For writers
+    // Use a ReentrantReadWriteLock to coordinate readers and writers correctly.
+    private static final ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
 
     static class Reader extends Thread {
         int id;
         Reader(int id) { this.id = id; }
 
         public void run() {
-            mutex.lock();
-            readCount++;
-            if (readCount == 1) writeLock.lock(); // First reader blocks writers
-            mutex.unlock();
-
-            System.out.println("Reader " + id + " is READING");
-
-            mutex.lock();
-            readCount--;
-            if (readCount == 0) writeLock.unlock(); // Last reader unblocks writers
-            mutex.unlock();
+            rwLock.readLock().lock();
+            try {
+                System.out.println("Reader " + id + " is READING");
+            } finally {
+                rwLock.readLock().unlock();
+            }
         }
     }
 
@@ -30,9 +23,12 @@ public class ReaderWriter {
         Writer(int id) { this.id = id; }
 
         public void run() {
-            writeLock.lock();
-            System.out.println("Writer " + id + " is WRITING");
-            writeLock.unlock();
+            rwLock.writeLock().lock();
+            try {
+                System.out.println("Writer " + id + " is WRITING");
+            } finally {
+                rwLock.writeLock().unlock();
+            }
         }
     }
 
